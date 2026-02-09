f148bb866fdee80d7c236e825123a3743d55b3c2
Pranay Kumar <kadivendipranay007@gmail.com>
2026-02-09 16:59:09 +0530

    Updated API base URL for production

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://restaurant-reservation-backend-6va5.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
